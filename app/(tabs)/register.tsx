43da7dce65f1a479736478e66d7e8d5edf7a65bf
monabarreiro <barreiro.monica@gmail>
2026-02-11 19:53:35 -0300

    background image para register

--- app/(tabs)/register.tsx
+++ app/(tabs)/register.tsx
@@ -48,7 +48,10 @@ export default function Register() {
   };
 
   return (
-    <ImageBackground source={require("./img/campo2.jpeg")} style={{ flex: 1 }}>
+    <ImageBackground
+      source={require("./img/campo2.jpeg")}
+      style={{ flex: 1, width: "100%", height: "100%" }}
+    >
       <View
         style={{
           alignItems: "center",
